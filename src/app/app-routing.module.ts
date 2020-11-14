27e0afab1d5b765476c3bc5447318b7bddda0de7
Benchari khaoula <khaoulabenchari9@gmail.com>
2020-11-14 21:46:20 +0100

    use routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ServiceComponent} from './service/service.component';
+import {ContactComponent} from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'services', component: ServiceComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceComponent } from './service/service.component';
-import { TeamComponent } from './team/team.component';
 
 import { FormsModule } from '@angular/forms';
 import { ContactComponent } from './contact/contact.component';
-import { TeamViewComponent } from './team-view/team-view.component';
-import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-  {path:'teams',component:TeamViewComponent},
-  { path: 'services', component: ServiceComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '', component: TeamViewComponent}
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ServiceComponent,
-    TeamComponent,
     ContactComponent,
-    TeamViewComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
